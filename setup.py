2c1d6c961a941f44060757515e1f2c46c1f696c6
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-10-20 17:53:00 +0530

    Add rl trainer and change version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if requirements_file.exists():
 
 setup(
     name="azcore",
-    version="0.0.2",
+    version="0.0.3",
     author="Azrienlabs team",
     author_email="info@azrianlabs.com",
     description="A professional hierarchical multi-agent framework built on python.",
